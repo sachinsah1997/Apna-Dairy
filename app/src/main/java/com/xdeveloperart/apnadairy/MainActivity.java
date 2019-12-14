5568b1a32c4dfc284e31a611e071eb20a4d0b745
sachinsah1997 <ssk1222014@gmail.com>
2019-12-14 12:28:00 +0530

    Initial Commit

---
+++ app/src/main/java/com/xdeveloperart/apnadairy/MainActivity.java
@@ -0,0 +1,14 @@
+package com.xdeveloperart.apnadairy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
