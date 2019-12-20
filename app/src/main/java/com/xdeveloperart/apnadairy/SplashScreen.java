e268e7c5595b0398c6dfd1a88baa7e554a9cb047
sachinsah1997 <ssk1222014@gmail.com>
2019-12-20 20:39:30 +0530

    commit from Android studio IDE till second commit registration.

---
+++ app/src/main/java/com/xdeveloperart/apnadairy/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.xdeveloperart.apnadairy;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splash_screen);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(1000);
+//                  sleep(100);
+                }
+                catch(InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    Intent openMain = new Intent(SplashScreen.this, AuthenticationActivity.class);
+                    startActivity(openMain);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
+
+
+
